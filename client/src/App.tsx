564a813b8aa71901e875d21d928f14c36d314ff9
Nickk91 <nickkaploon@gmail.com>
2025-02-15 10:54:31 +0200

    added axios and Auth logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,24 @@
-import { Button } from "./components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage.tsx";
+import AuthCallbackPage from "./pages/AuthCallbackPage/AuthCallbackPage.tsx";
+import { axiosInstance } from "./lib/axios.ts";
+
 function App() {
+  const getSomeData = async () => {
+    const res = await axiosInstance.get("/users", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(res);
+  };
+
   return (
     <>
-      <header>
-        <SignedOut>
-          <SignInButton>
-            <Button>Sign In</Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/providers/AuthProvider.tsx
@@ -0,0 +1,50 @@
+import { axiosInstance } from "@/lib/axios";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { Loader } from "lucide-react";
+
+// Update the axios default header with the provided token
+const updateApiToken = (token: string | null) => {
+  if (token) {
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axiosInstance.defaults.headers.common["Authorization"];
+  }
+};
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const { getToken } = useAuth(); // Removed unused 'userId'
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      try {
+        const token = await getToken();
+        updateApiToken(token);
+      } catch (error: any) {
+        updateApiToken(null);
+        console.error("Error in auth provider:", error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    initAuth();
+  }, [getToken]);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-full flex items-center justify-center">
+        <Loader className="w-8 h-8 text-emerald-500 animate-spin" />
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+};
+
+export default AuthProvider;
