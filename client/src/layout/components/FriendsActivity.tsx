a9f94c38939dae53eda70cf6abd779eec8c3f7ed
Nickk91 <nickkaploon@gmail.com>
2025-02-19 21:17:20 +0200

    fixed some bugs

--- client/src/layout/components/FriendsActivity.tsx
+++ client/src/layout/components/FriendsActivity.tsx
@@ -1,13 +1,12 @@
 import { useChatStore } from "@/stores/useChatStore.ts";
-import { Divide, HeadphonesIcon, Music } from "lucide-react";
+import { Users, HeadphonesIcon, Music } from "lucide-react";
 import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
-import { Users } from "lucide-react";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const FriendsActivity = () => {
-  const { users, isLoading, error, fetchUsers } = useChatStore();
+  const { users, fetchUsers } = useChatStore();
 
   const { user } = useUser();
   useEffect(() => {
@@ -29,46 +28,49 @@ const FriendsActivity = () => {
 
       <ScrollArea className="flex-1">
         <div className="p-4 space-y-4">
-          <div
-            key={user?.id}
-            className="cursor-pointer hover:bg-zinc-800/50 p-3 rounded-md transition-colors group"
-          >
-            <div className="flex items-start gap-3 ">
-              <div className="relative ">
-                <Avatar className="size-10 border border-zinc-800">
-                  <AvatarImage src={user?.imageUrl} alt={user?.fullName} />
-                  <AvatarFallback>{user?.fullName[0]}</AvatarFallback>
-                </Avatar>
-                <div
-                  className="absolute bottom-0 right-0 w-3 h-3 rounded-full border-2 bg-zinc-900"
-                  aria-hidden="true"
-                ></div>
-                <div className="flex-1 min-w-0">
-                  <div className="flex items-center gap-2">
-                    <span className="font-medium text-sm text-white">
-                      {user?.fullName}
-                    </span>
-                    {isPlaying && (
-                      <Music className="size-3.5 text-emerald-400 shrink-0" />
+          {users.map((user) => (
+            <div
+              key={user._id}
+              className="cursor-pointer hover:bg-zinc-800/50 p-3 rounded-md transition-colors group"
+            >
+              <div className="flex items-start gap-3 ">
+                <div className="relative ">
+                  <Avatar className="size-10 border border-zinc-800">
+                    <AvatarImage src={user?.imageUrl} alt={user?.fullName} />
+                    <AvatarFallback>{user.fullName[0]}</AvatarFallback>
+                  </Avatar>
+                  <div
+                    className="absolute bottom-0 right-0 w-3 h-3 rounded-full border-2 bg-zinc-900"
+                    aria-hidden="true"
+                  ></div>
+
+                  <div className="flex-1 min-w-0">
+                    <div className="flex items-center gap-2">
+                      <span className="font-medium text-sm text-white">
+                        {user?.fullName}
+                      </span>
+                      {isPlaying && (
+                        <Music className="size-3.5 text-emerald-400 shrink-0" />
+                      )}
+                    </div>
+
+                    {isPlaying ? (
+                      <div className="mt-1 text-xs">
+                        <div className="mt-1 text-sm text-white font-medium truncate">
+                          Cardigan
+                        </div>
+                        <div className="text-xs text-zinc-400 truncate">
+                          by Taylor Swift
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="mt-1 text-xs text-zinc-400">Idle</div>
                     )}
                   </div>
-
-                  {isPlaying ? (
-                    <div className="mt-1 text-xs">
-                      <div className="mt-1 text-sm text-white font-medium truncate">
-                        Cardigan
-                      </div>
-                      <div className="text-xs text-zinc-400 truncate">
-                        by Taylor Swift
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="mt-1 text-xs text-zinc-400">Idle</div>
-                  )}
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </ScrollArea>
     </div>
