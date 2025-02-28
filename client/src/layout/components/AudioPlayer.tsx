c11061418a3f75cbe4a41fd4b9586a62b3bf2e8d
Nickk91 <nickkaploon@gmail.com>
2025-02-28 14:31:28 +0200

    Added album page with functionality

--- client/src/layout/components/AudioPlayer.tsx
+++ client/src/layout/components/AudioPlayer.tsx
@@ -3,52 +3,46 @@ import { useEffect, useRef } from "react";
 
 const AudioPlayer = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
-  const prevSongRef = useRef<string|null>(null)>
+  const prevSongRef = useRef<string | null>(null);
 
-  const{ currentSong, isPlaying,playNext }= usePlayerStore()
-
+  const { currentSong, isPlaying, playNext } = usePlayerStore();
 
   //handle play/pause logic
   useEffect(() => {
-    if(isPlaying) audioRef.current?.play();
+    if (isPlaying) audioRef.current?.play();
     else audioRef.current?.pause();
-  }, [isPlaying])
+  }, [isPlaying]);
 
   //handle song ends
   useEffect(() => {
     const audio = audioRef.current;
 
     const handleEnded = () => {
-        playNext();
-
-    }
+      playNext();
+    };
     audio?.addEventListener("ended", handleEnded);
 
     return () => audio?.removeEventListener("ended", handleEnded);
-
-  }, [playNext])
-
+  }, [playNext]);
 
   //handle song changes
   useEffect(() => {
-    if(!audioRef.current|| !currentSong) return;
+    if (!audioRef.current || !currentSong) return;
     const audio = audioRef.current;
 
     //check if this is actually a new song
-    const isSongChange = prevSongRef.current !== currentSong?.audioUrl
-      if(isSongChange){
-        audio.src = currentSong?.audioUrl;
-        //reset the playback position
-        audio.currentTime = 0;
+    const isSongChange = prevSongRef.current !== currentSong?.audioUrl;
+    if (isSongChange) {
+      audio.src = currentSong?.audioUrl;
+      //reset the playback position
+      audio.currentTime = 0;
 
-        prevSongRef.current = currentSong?.audioUrl    
-        if(isPlaying) audio.play();
-      }
-    
+      prevSongRef.current = currentSong?.audioUrl;
+      if (isPlaying) audio.play();
+    }
   }, [currentSong, isPlaying]);
 
-
-  return (<audio ref={audioRef} />);
+  return <audio ref={audioRef} />;
 };
 
 export default AudioPlayer;
